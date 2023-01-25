838745a229f6b4546cb4ff6330010fd33c568b68
Niraj Bera <beraniraj2503@gmail.com>
2023-01-25 17:05:04 +0530

    add array.php

---
+++ constants.php
@@ -0,0 +1,39 @@
+<html>
+    <?php
+        //Constants are like variables except that once they are defined they cannot be changed or undefined
+        //constant is an identifier (name) for a simple value. The value cannot be changed during the script
+
+
+        define("cons","This is php constanst example");
+        echo cons;
+
+        //it use in case-sensitive use but pass aggrunment true
+        // echo CONS;
+        echo "<br>";
+        //array constants
+        define("name", [
+            "raj",
+            "jay",
+            "meet"
+          ]);
+          echo name[0];
+
+          echo "<br>";
+
+          //globle use
+          function myfun() {
+            echo cons;
+          }
+           
+          myfun();
+
+
+
+          //
+          $a=20;
+          $a=25;
+          define("cons","This is php constanst");
+
+    ?>
+
+</html>
